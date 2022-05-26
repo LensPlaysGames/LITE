3acf6c6fbc898b983855a9a6d6b543f956925266
Lens <LensPlaysGames@gmail.com>
2022-05-26 16:29:31 -0700

    Complete REPL
    Reading, evaluation, printing, and looping
    are all things that LITE now can do!

--- src/repl.c
+++ src/repl.c
@@ -2,10 +2,67 @@
 
 #include <environment.h>
 #include <error.h>
+#include <parser.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <types.h>
+
+//================================================================ BEG evaluation
+
+int evaluate_expr(Atom expr, Atom environment, Atom *result) {
+  Atom operator;
+  Atom arguments;
+  Atom arguments_it;
+  enum Error err;
+  if (expr.type == ATOM_TYPE_SYMBOL) {
+    return env_get(environment, expr, result);
+  } else if (expr.type != ATOM_TYPE_PAIR) {
+    *result = expr;
+    return ERROR_NONE;
+  }
+  if (!listp(expr)) {
+    return ERROR_SYNTAX;
+  }
+  operator = car(expr);
+  arguments = cdr(expr);
+  if (operator.type == ATOM_TYPE_SYMBOL) {
+    if (memcmp(operator.value.symbol, "QUOTE", 5) == 0) {
+      if (nilp(arguments) || !nilp(cdr(arguments))) {
+        return ERROR_ARGUMENTS;
+      }
+      *result = car(arguments);
+      return ERROR_NONE;
+    } else if (memcmp(operator.value.symbol, "DEFINE", 6) == 0) {
+      Atom symbol;
+      Atom value;
+      // Ensure two arguments.
+      if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
+        return ERROR_ARGUMENTS;
+      }
+      symbol = car(arguments);
+      if (symbol.type != ATOM_TYPE_SYMBOL) {
+        return ERROR_TYPE;
+      }
+      err = evaluate_expr(car(cdr(arguments)), environment, &value);
+      if (err) { return err; }
+      *result = symbol;
+      return env_set(environment, symbol, value);
+    }
+  }
+  err = evaluate_expr(operator, environment, &operator);
+  if (err) { return err; }
+  arguments = copy_list(arguments);
+  arguments_it = arguments;
+  while (!nilp(arguments_it)) {
+    err = evaluate_expr(car(arguments_it), environment, &car(arguments_it));
+    if (err) { return err; }
+    arguments_it = cdr(arguments_it);
+  }
+  return apply(operator, arguments, result);
+}
+
+//================================================================ END evaluation
 
 static const char *repl_prompt = "lite|> ";
 static char user_input[MAX_INPUT_BUFSZ];
@@ -29,17 +86,33 @@ void enter_repl() {
   while (1) {
     //==== READ ====
     // Get current input as heap-allocated C string.
-    char *line = readline();
-    //==== EVAL ====
-    // If any line starts with "quit", exit REPL.
-    if (memcmp(line, "quit", 4) == 0) {
+    char *input = readline();
+    const char *source = input;
+    // If any input starts with "quit", exit REPL.
+    if (memcmp(source, "quit", 4) == 0) {
       break;
     }
+    enum Error err;
+    Atom expr;
+    err = parse_expr(source, &source, &expr);
+    if (err) {
+      print_error(err);
+      return;
+    }
+    //==== EVAL ====
+    Atom result;
+    err = evaluate_expr(expr, environment, &result);
     //==== PRINT ====
-    // For now, just loop back input.
-    printf("%s\n", line);
+    switch (err) {
+    case ERROR_NONE:
+      print_atom(result);
+      putchar('\n');
+      break;
+    default:
+      print_error(err);
+    }
     //==== LOOP ====
     // Free heap-allocated memory.
-    free(line);
+    free(input);
   }
 }
