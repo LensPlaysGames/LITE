6461f25b22bca54a8171b486853def3e85e82f62
Lens <lensplaysgames@gmail.com>
2022-06-25 13:38:57 -0700

    BuiltIn argument ensurance macros
    This makes both writing and reading builtins much easier.

--- src/builtins.c
+++ src/builtins.c
@@ -7,10 +7,39 @@
 #include <string.h>
 #include <types.h>
 
+#define BUILTIN_ENSURE_NO_ARGUMENTS(builtin_args) do {  \
+    if (!nilp(builtin_args)) {                          \
+      return ERROR_ARGUMENTS;                           \
+    }                                                   \
+  } while (0);
+
+#define BUILTIN_ENSURE_ONE_ARGUMENT(builtin_args) do {      \
+    if (nilp(builtin_args) || !nilp(cdr(builtin_args))) {   \
+      return ERROR_ARGUMENTS;                               \
+    }                                                       \
+  } while (0);
+
+#define BUILTIN_ENSURE_TWO_ARGUMENTS(builtin_args) do { \
+    if (nilp(builtin_args)                              \
+        || nilp(cdr(builtin_args))                      \
+        || !nilp(cdr(cdr(builtin_args))))               \
+      {                                                 \
+        return ERROR_ARGUMENTS;                         \
+      }                                                 \
+  } while (0);
+
+#define BUILTIN_ENSURE_THREE_ARGUMENTS(builtin_args) do {   \
+    if (nilp(builtin_args)                                  \
+        || nilp(cdr(builtin_args))                          \
+        || nilp(cdr(cdr(builtin_args)))                     \
+        || !nilp(cdr(cdr(cdr(builtin_args)))))              \
+      {                                                     \
+        return 0;                                           \
+      }                                                     \
+  } while (0);
+
 int typep(Atom arguments, enum AtomType type, Atom *result) {
-  if (nilp(arguments) || !nilp(cdr(arguments))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_ONE_ARGUMENT(arguments);
   *result = car(arguments).type == type ? make_sym("T") : nil;
   return ERROR_NONE;
 }
@@ -76,9 +105,7 @@ int builtin_bufferp(Atom arguments, Atom *result) {
 symbol_t *builtin_not_docstring =
   "Given ARG is nil, return 'T', otherwise return nil.";
 int builtin_not(Atom arguments, Atom *result) {
-  if (nilp (arguments) || !nilp(cdr(arguments))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_ONE_ARGUMENT(arguments);
   *result = nilp(car(arguments)) ? make_sym("T") : nil;
   return ERROR_NONE;
 }
@@ -86,9 +113,7 @@ int builtin_not(Atom arguments, Atom *result) {
 symbol_t *builtin_car_docstring =
   "Given ARG is a pair, return the value on the left side.";
 int builtin_car(Atom arguments, Atom *result) {
-  if (nilp(arguments) || !nilp(cdr(arguments))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_ONE_ARGUMENT(arguments);
   if (nilp(car(arguments))) {
     *result = nil;
   } else if (car(arguments).type != ATOM_TYPE_PAIR) {
@@ -102,9 +127,7 @@ int builtin_car(Atom arguments, Atom *result) {
 symbol_t *builtin_cdr_docstring =
   "Given ARG is a pair, return the value on the right side.";
 int builtin_cdr(Atom arguments, Atom *result) {
-  if (nilp(arguments) || !nilp(cdr(arguments))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_ONE_ARGUMENT(arguments);
   if (nilp(car(arguments))) {
     *result = nil;
   } else if (car(arguments).type != ATOM_TYPE_PAIR) {
@@ -118,9 +141,7 @@ int builtin_cdr(Atom arguments, Atom *result) {
 symbol_t *builtin_cons_docstring =
   "Return a new pair, with LEFT and RIGHT on each side, respectively.";
 int builtin_cons(Atom arguments, Atom *result) {
-  if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   *result = cons(car(arguments), car(cdr(arguments)));
   return ERROR_NONE;
 }
@@ -128,9 +149,7 @@ int builtin_cons(Atom arguments, Atom *result) {
 symbol_t *builtin_add_docstring =
   "Add two integer numbers A and B together, and return the computed result.";
 int builtin_add(Atom arguments, Atom *result) {
-  if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -143,9 +162,7 @@ int builtin_add(Atom arguments, Atom *result) {
 symbol_t *builtin_subtract_docstring =
   "Subtract integer B from integer A and return the computed result.";
 int builtin_subtract(Atom arguments, Atom *result) {
-  if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -158,9 +175,7 @@ int builtin_subtract(Atom arguments, Atom *result) {
 symbol_t *builtin_multiply_docstring =
   "Multiply integer numbers A and B together and return the computed result.";
 int builtin_multiply(Atom arguments, Atom *result) {
-  if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -173,9 +188,7 @@ int builtin_multiply(Atom arguments, Atom *result) {
 symbol_t *builtin_divide_docstring =
   "Divide integer B out of integer A and return the computed result.";
 int builtin_divide(Atom arguments, Atom *result) {
-  if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -191,9 +204,7 @@ int builtin_divide(Atom arguments, Atom *result) {
 symbol_t *builtin_buffer_table_docstring =
   "Return the LISP buffer table.";
 int builtin_buffer_table(Atom arguments, Atom *result) {
-  if (!nilp(arguments)) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_NO_ARGUMENTS(arguments);
   *result = *buf_table();
   return ERROR_NONE;
 }
@@ -203,12 +214,7 @@ symbol_t *builtin_buffer_insert_docstring =
 \
 Insert STRING into BUFFER at point.";
 int builtin_buffer_insert(Atom arguments, Atom *result) {
-  if (nilp(arguments)
-      || nilp(cdr(arguments))
-      || !nilp(cdr(cdr(arguments))))
-    {
-      return ERROR_ARGUMENTS;
-    }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom buffer = car(arguments);
   Atom string = car(cdr(arguments));
   if (!bufferp(buffer) || !stringp(string)) {
@@ -229,12 +235,7 @@ symbol_t *builtin_buffer_remove_docstring =
 \
 Backspace COUNT bytes from BUFFER at point.";
 int builtin_buffer_remove(Atom arguments, Atom *result) {
-  if (nilp(arguments)
-      || nilp(cdr(arguments))
-      || !nilp(cdr(cdr(arguments))))
-    {
-      return ERROR_ARGUMENTS;
-    }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom buffer = car(arguments);
   Atom count = car(cdr(arguments));
   if (!bufferp(buffer) || !integerp(count)) {
@@ -255,9 +256,7 @@ symbol_t *builtin_numeq_docstring =
 \
 Return 'T' iff the two given arguments have the same integer value.";
 int builtin_numeq(Atom arguments, Atom *result) {
-  if (nilp (arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -272,9 +271,7 @@ symbol_t *builtin_numnoteq_docstring =
 \
 Return 'T' iff the two given arguments *do not* have the same integer value.";
 int builtin_numnoteq(Atom arguments, Atom *result) {
-  if (nilp (arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -289,9 +286,7 @@ symbol_t *builtin_numlt_docstring =
 \
 Return 'T' iff integer A is less than integer B.";
 int builtin_numlt(Atom arguments, Atom *result) {
-  if (nilp (arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -306,9 +301,7 @@ symbol_t *builtin_numlt_or_eq_docstring =
 \
 Return 'T' iff integer A is less than or equal to integer B.";
 int builtin_numlt_or_eq(Atom arguments, Atom *result) {
-  if (nilp (arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -323,9 +316,7 @@ symbol_t *builtin_numgt_docstring =
 \
 Return 'T' iff integer A is greater than integer B.";
 int builtin_numgt(Atom arguments, Atom *result) {
-  if (nilp (arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -340,9 +331,7 @@ symbol_t *builtin_numgt_or_eq_docstring =
 \
 Return 'T' iff integer A is greater than or equal to integer B.";
 int builtin_numgt_or_eq(Atom arguments, Atom *result) {
-  if (nilp (arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom lhs = car(arguments);
   Atom rhs = car(cdr(arguments));
   if (lhs.type != ATOM_TYPE_INTEGER || rhs.type != ATOM_TYPE_INTEGER) {
@@ -357,9 +346,7 @@ symbol_t *builtin_save_docstring =
 \
 Save the given BUFFER to a file.";
 int builtin_save(Atom arguments, Atom *result) {
-  if (nilp(arguments) || !nilp(cdr(arguments))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_ONE_ARGUMENT(arguments);
   Atom buffer = car(arguments);
   if (!bufferp(buffer)) {
     return ERROR_TYPE;
@@ -375,9 +362,7 @@ int builtin_save(Atom arguments, Atom *result) {
 symbol_t *builtin_apply_docstring =
   "Call FUNCTION with the given ARGUMENTS and return the result.";
 int builtin_apply(Atom arguments, Atom *result) {
-  if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom function;
   function = car(arguments);
   arguments = car(cdr(arguments));
@@ -425,9 +410,7 @@ int builtin_apply(Atom arguments, Atom *result) {
 symbol_t *builtin_eq_docstring =
   "Return 'T' iff A and B refer to the same Atomic LISP object.";
 int builtin_eq(Atom arguments, Atom *result) {
-  if (nilp(arguments) || nilp(cdr(arguments)) || !nilp(cdr(cdr(arguments)))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_TWO_ARGUMENTS(arguments);
   Atom a = car(arguments);
   Atom b = car(cdr(arguments));
   int equal = 0;
@@ -465,9 +448,7 @@ int builtin_eq(Atom arguments, Atom *result) {
 symbol_t *builtin_symbol_table_docstring =
   "Return the LISP symbol table.";
 int builtin_symbol_table(Atom arguments, Atom *result) {
-  if (!nilp(arguments)) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_NO_ARGUMENTS(arguments);
   *result = *sym_table();
   return ERROR_NONE;
 }
@@ -475,9 +456,7 @@ int builtin_symbol_table(Atom arguments, Atom *result) {
 symbol_t *builtin_print_docstring =
   "Print the given ARG to standard out, prettily.";
 int builtin_print(Atom arguments, Atom *result) {
-  if (nilp(arguments) || !nilp(cdr(arguments))) {
-    return ERROR_ARGUMENTS;
-  }
+  BUILTIN_ENSURE_ONE_ARGUMENT(arguments);
   pretty_print_atom(car(arguments));
   putchar('\n');
   *result = nil;
